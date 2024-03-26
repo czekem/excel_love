389e10f08e2142ded3ac05f2655def29cceb273c
Michael Czekaj <145675402+czekem@users.noreply.github.com>
2024-03-26 03:38:00 +0100

    Update tests.py

--- tests.py
+++ tests.py
@@ -20,3 +20,24 @@ def test_match_list_pattern():
     question = 'list'
     result = pattern.match(question)
     assert result is not None
+
+
+def test_entering_wrong_option():
+    pattern = re.compile(r'(check|check if file exist|check for file|list)$')
+    question = 'wrong'
+    result = pattern.match(question)
+    assert result is None
+
+
+def test_enter_looking_file():
+    with tempfile.NamedTemporaryFile(suffix='.txt', delete=False) as temp:
+        temp_file_path = temp.name
+        
+    name = 'alice'
+    first_question = '.txt'
+
+    test_enter_looking_file = glob.glob(name + first_question)
+    
+    os.remove(temp_file_path)
+
+    assert test_enter_looking_file is not None
