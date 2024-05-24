0908c0995f0c5ed6df9bceff4087212fe048df8d
Michael Czekaj <145675402+czekem@users.noreply.github.com>
2024-05-24 14:07:06 +0200

    Update speech.py

--- data_crusher/speech.py
+++ data_crusher/speech.py
@@ -1,1 +1,37 @@
+from dataclasses import dataclass
+import os 
+import glob
+import csv
+import json
+import re
 
+import sys
+import pathlib
+from pathlib import Path
+
+from matplotlib import figure
+import pandas as pd
+ 
+import matplotlib.pyplot as plt 
+import plotly.express as px
+import plotly.io as pio
+
+import click
+import openpyxl
+import inquirer
+import numpy as np
+
+from openpyxl.utils import get_column_letter
+from openpyxl.worksheet.dimensions import ColumnDimension
+import plotly.graph_objects as go
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import click
+import pyttsx3
+import tkinter as tk
+
+engine = pyttsx3.init()
+engine.setProperty('rate', 160)
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
