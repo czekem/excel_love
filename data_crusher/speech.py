821e5a06b5318e10799862ba02ca4dffdc5a11c1
Michael Czekaj <145675402+czekem@users.noreply.github.com>
2024-06-01 08:43:56 +0200

    Update speech.py

--- data_crusher/speech.py
+++ data_crusher/speech.py
@@ -41,6 +41,8 @@ def speak(text):
     engine.say(text)
     engine.runAndWait()
 
+
+
 @dataclass
 class WorkFile:
     def __init__(self, name, extension, df):
@@ -61,7 +63,6 @@ class WorkFile:
         except Exception as e:
             print(e)
             return self.looking_for_file()
-
 
     def looking_for_file(self, max_attempts=3):
         name = f'{self.name}.{self.extension}'
@@ -87,3 +88,42 @@ class WorkFile:
                         print(f'The file {name} exists in the folder {root}')
                         found_files.append(os.path.join(root, name))
                         break  # Break the loop as we found the file
+
+            attempts += 1
+
+        if len(found_files) > 1:
+            print("Multiple files found. Please select one:")
+            for i, file_path in enumerate(found_files, start=1):
+                print(f"{i}. {file_path}")
+            selection = input('Please enter the number of the file you want to open: ')
+            return found_files[int(selection) - 1]
+        elif len(found_files) == 1:
+            return found_files[0]  # Return the first file if only one is found
+        else:
+            print(f'File {name} not found in the system after {max_attempts} attempts. Exiting the program.')
+            sys.exit()
+
+
+
+    def read_excel_file(self):
+        file_path = self.open_file()
+        if self.extension == 'xlsx':
+            print("Available worksheets:")
+            with pd.ExcelFile(file_path) as xls:
+                for sheet in xls.sheet_names:
+                    print(sheet)
+            sheet_name = input("Please enter the name of the worksheet you want to use: ")
+            df = pd.read_excel(file_path, sheet_name=sheet_name, engine='openpyxl')
+            return df
+        else:
+            pass
+
+    def open_the_file(self):
+        pass
+
+    def close_the_file(self):
+        pass
+
+class OpenFile(WorkFile):
+    def __init__(self, df):
+        self.df = df
